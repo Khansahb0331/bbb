4f90aa0a09d6e146ba7de17aac1feeeb5155aa9c
Muhammad Ahmed <94356875+Khansahb0331@users.noreply.github.com>
2024-01-17 16:05:11 +0500

    khan

---
+++ index.ts
@@ -0,0 +1,11 @@
+// 3 types variable
+//string
+let prosonename:string="sultan";
+//number
+let age:number=22;
+//boolen
+let alive:boolean=true;
+//output
+console.log(personname);
+console.log(age);
+console.log(alive);
